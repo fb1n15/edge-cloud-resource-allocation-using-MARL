76fdbe252dd1088f4bf8b7bcdd28fd7eccb87a89
Jack Parsons <jack.parsons.uk@icloud.com>
2021-04-10 00:42:17 +0100

    Adding convolutional model experiments to scaling up

--- run_scaling_up.sh
+++ run_scaling_up.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks-per-node=14
 #SBATCH --nodes=1
 #SBATCH --partition=lycium
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jp6g18@soton.ac.uk
@@ -11,6 +11,12 @@ case $SLURM_ARRAY_TASK_ID in
 # Scaling up experiment
   1)
     CONFIG_FILE="configs/experiments/scaling_up/ppo_fc_independent.yaml"
+    ;;
+  2)
+    CONFIG_FILE="configs/experiments/scaling_up/ppo_convolutional_independent.yaml"
+    ;;
+  2)
+    CONFIG_FILE="configs/experiments/scaling_up/ppo_convolutional_centralised.yaml"
     ;;
 esac
 
