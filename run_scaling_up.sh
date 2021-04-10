4db1b8e341f6f9052e4efa36527ff9244f63035a
Jack Parsons <jack.parsons.uk@icloud.com>
2021-04-10 01:08:38 +0100

    Lowering time required

--- run_scaling_up.sh
+++ run_scaling_up.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks-per-node=14
 #SBATCH --nodes=1
 #SBATCH --partition=lycium
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jp6g18@soton.ac.uk
