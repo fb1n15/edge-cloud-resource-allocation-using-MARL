c31caab77f28ee431ae6ae60232d6d9b7abaa2f3
Jack Parsons <jack.parsons.uk@icloud.com>
2021-04-20 23:39:13 +0100

    Updating unceratinty experiment

--- run_fc_depth.sh
+++ run_fc_depth.sh
@@ -24,6 +24,9 @@ case $SLURM_ARRAY_TASK_ID in
   5)
     CONFIG_FILE="configs/experiments/fc_depth/ppo_fc_independent_depth5.yaml"
     ;;
+  6)
+    CONFIG_FILE="configs/experiments/fc_depth/ppo_fc_independent_lr_depth2.yaml"
+    ;;
 esac
 
 echo "Starting Job"
