75b31e50ba86ab55238dc486fc5d8cc4ffad578b
Chandler <fb1n15@users.noreply.github.com>
2021-10-26 23:16:03 +0100

    added a bash file to run trials with different number of actions

--- run_with_different_n_actions.sh
+++ run_with_different_n_actions.sh
@@ -3,9 +3,10 @@
 #SBATCH --output=/mainfs/home/fb1n15/MARL-ReverseAuction/marl-edge-cloud/iridis-reports/%j.out
 #SBATCH --ntasks=12  # Number of Tasks (up-to 32 jobs running at the same time)
 #SBATCH --cpus-per-task=10  # use multiple cores each for multithreading
-#SBATCH --time=4:00:00
+#SBATCH --time=2:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=fan_bi@icloud.com
+
 # (https://stackoverflow.com/a/67537416/7060068)
 # #SBATCH --nodes=2  # Number of nodes requested
 # #SBATCH --exclusive          # I don't want to share my compute node with anyone
@@ -16,6 +17,10 @@ n_tasks=4
 
 CONFIG_FILE1="/mainfs/home/fb1n15/MARL-ReverseAuction/marl-edge-cloud/configs/experiments/edge_cloud/hyperparameters/cpu_ppo_fc_independent_with_history.yaml"
 
+CONFIG_FILE2="/mainfs/home/fb1n15/MARL-ReverseAuction/marl-edge-cloud/configs/experiments/edge_cloud/hyperparameters/cpu_ppo_fc_independent_with_history.yaml"
+
+CONFIG_FILE3="/mainfs/home/fb1n15/MARL-ReverseAuction/marl-edge-cloud/configs/experiments/edge_cloud/hyperparameters/cpu_ppo_fc_independent_with_history.yaml"
+
 echo "Starting Job"
 
 module load conda/py3-latest
@@ -24,5 +29,7 @@ module load openmpi/3.0.0/intel
 export PYTHONPATH="${PYTHONPATH}:${SLURM_SUBMIT_DIR}/src"
 
 mpirun -np $n_tasks --bind-to none python ./src/marl.py train --config $CONFIG_FILE1
+mpirun -np $n_tasks --bind-to none python ./src/marl.py train --config $CONFIG_FILE2
+mpirun -np $n_tasks --bind-to none python ./src/marl.py train --config $CONFIG_FILE3
 
 echo "Finishing job"
