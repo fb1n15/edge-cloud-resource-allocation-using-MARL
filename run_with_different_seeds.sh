b1d8e891a9bf80a49dfe66e3c4278ab330aa58c7
Chandler <fb1n15@users.noreply.github.com>
2021-10-26 15:45:31 +0100

    save the slurm output.out file at iridis-reports folder

--- run_with_different_seeds.sh
+++ run_with_different_seeds.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-
+# #SBATCH --nodes=2  # Number of nodes requested
+#SBATCH --ntasks=10  # Number of Tasks
 #SBATCH --ntasks-per-node=4  # Tasks per node
-#SBATCH --nodes=2  # Number of nodes requested
 #SBATCH --partition=batch
 #SBATCH --time=2:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=fan_bi@icloud.com
+#SBATCH --output=/mainfs/home/fb1n15/MARL-ReverseAuction/marl-edge-cloud/iridis-reports/%j.out
+# (https://stackoverflow.com/a/67537416/7060068)
+
+
+cd "$HOME"/MARL-ReverseAuction/marl-edge-cloud/ || exit  # cd to the project location
 
 case $SLURM_ARRAY_TASK_ID in
 # Scaling up experiment
@@ -22,11 +27,13 @@ echo "Starting Job"
 
 module load conda/py3-latest
 source activate jack
+module load openmpi/3.0.0/intel
 export PYTHONPATH="${PYTHONPATH}:${SLURM_SUBMIT_DIR}/src"
-for j in {1..10}
-do
-  python ./src/marl.py train --config $CONFIG_FILE
-done
+#for j in {1..10}
+#do
+#  python ./src/marl.py train --config $CONFIG_FILE
+#done
 
+mpirun -np 10 python ./src/marl.py train --config $CONFIG_FILE
 
 echo "Finishing job"
