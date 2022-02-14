e6839b119b128423c912c162ce1b0cb995a948a6
Chandler B <Fan.Bi@soton.ac.uk>
2022-02-14 17:26:07 +0000

    seed generator
    now the seed generator will use a fixed seed every time.

--- config_generator.py
+++ config_generator.py
@@ -1,15 +1,22 @@
 # read the config template and modify it to generate the config file used in the experiment
+import random  # for generating random numbers
 
-import yaml
-from pprint import pprint
+import yaml  # for reading the config template
+from pprint import pprint  # for printing the config file
 
 with open("./configs/config_template.yaml", 'r') as stream:
     config = yaml.load(stream, Loader=yaml.FullLoader)
-    print("config: ")
-    pprint(config)
+    # print("config: ")
+    # pprint(config)
+
+seed = 1
+random.seed(seed)  # set the seed for the random number generator
 
 # modify the config template
-config['env-config']['seed'] = 777
+config['env-config']['seed'] = random.randint(0, 10000)
+print("environment seed: ", config['env-config']['seed'])
+config['trainer-config']['seed'] = random.randint(0, 10000)
+print("RL algorithm seed: ", config['trainer-config']['seed'])
 config['stop']['timesteps_total'] = 100
 
 # write the config file
