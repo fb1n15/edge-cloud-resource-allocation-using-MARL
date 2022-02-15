a07ed4294235fc87a9cee095b4297c74175736f5
Chandler B <Fan.Bi@soton.ac.uk>
2022-02-15 10:40:33 +0000

    done

--- config_generator.py
+++ config_generator.py
@@ -17,7 +17,9 @@ config['env-config']['seed'] = random.randint(0, 10000)
 print("environment seed: ", config['env-config']['seed'])
 config['trainer-config']['seed'] = random.randint(0, 10000)
 print("RL algorithm seed: ", config['trainer-config']['seed'])
-config['stop']['timesteps_total'] = 100
+config['trainer-config']['train_batch_size']['gridsearch'] = [1000]
+config['stop']['timesteps_total'] = 3000
+
 
 # write the config file
 with open("./configs/config_local.yaml", 'w') as outfile:
